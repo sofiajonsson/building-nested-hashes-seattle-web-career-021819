2d8f7d4256fe0059e8a8cd2677e874f762ae4d90
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-04 05:39:04 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,20 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
+    epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
       #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
+     
 
   
 end
