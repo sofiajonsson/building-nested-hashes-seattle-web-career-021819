dfae1b035d0ae6e9f30e8f2895da6f0d08699549
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-04 05:29:00 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -20,8 +20,9 @@ epic_tragedy = {
    }
 }
 
- epic_tragedy[:montague][:hero][:status] = "dead"
-  puts epic_tragedy
- epic_tragedy[:capulet][:heroine][:status] = "dead"
- puts epic_tragedy
+ 
 end
+
+ epic_tragedy[:montague][:hero][:status] = "dead"
+ 
+ epic_tragedy[:capulet][:heroine][:status] = "dead"
